f86175ce5b62abc17b70a1dd6ae4e028d5919d3d
Cédric L’homme <public@l-homme.com>
2022-06-24 17:42:45 -0400

    add texts

--- main.go
+++ main.go
@@ -7,9 +7,13 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
 	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/hajimehoshi/ebiten/v2/text"
 	"github.com/jakecoffman/cp"
 	"golang.org/x/image/colornames"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
 
 	"log"
 )
@@ -24,6 +28,7 @@ const (
 	ballSize     = 5
 	magSize      = 5
 	bellSize     = 20
+	scoreMargin  = 5
 	drawVelocity = false
 
 	// Physics
@@ -67,12 +72,51 @@ var (
 	bellImage = ebiten.NewImage(bellSize, bellSize)
 	colorBlue = color.RGBA{B: 255, A: 1}
 	colorRed  = color.RGBA{R: 255, A: 1}
+
+	titleArcadeFont font.Face
+	arcadeFont      font.Face
+	smallArcadeFont font.Face
+	fontSize        = 24
+	titleFontSize   = int(float64(fontSize) * 1.5)
+	smallFontSize   = fontSize / 2
 )
 
 func init() {
+	// Images
 	ballImage.Fill(color.White)
 	magImage.Fill(colorBlue)
 	bellImage.Fill(colorRed)
+
+	// Fonts
+	tt, err := opentype.Parse(fonts.PressStart2P_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	const dpi = 72
+	titleArcadeFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    float64(titleFontSize),
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	arcadeFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    float64(fontSize),
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	smallArcadeFont, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    float64(smallFontSize),
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func main() {
@@ -308,6 +352,50 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	op.GeoM.Translate(g.bell.Position().X-bellSize/2, g.bell.Position().Y-bellSize/2)
 	screen.DrawImage(bellImage, op)
 
+	// Texts
+	// Ref: https://github.com/hajimehoshi/ebiten/blob/main/examples/flappy/main.go
+	var titleTexts []string
+	var texts []string
+	switch gamestate {
+	case GameReady:
+		titleTexts = []string{"MagLab"}
+		texts = []string{"", "", "", "", "", "", "",
+			"PRESS SPACE KEY", "TO START", "THE PREPARATION PERIOD",
+			fmt.Sprintf("(%d seconds)", preparationDuration)}
+	case GamePreparing:
+		t := gamePreparingTimeout - g.time
+		if t > 0.1 {
+			texts = []string{"", "", "", "", "", "", fmt.Sprintf("%.1f", t)}
+		}
+	case GameEnded:
+		texts = []string{"", "GAME OVER!"}
+	}
+	for i, l := range titleTexts {
+		x := (screenWidth - len(l)*titleFontSize) / 2
+		text.Draw(screen, l, titleArcadeFont, x, (i+4)*titleFontSize, color.White)
+	}
+	for i, l := range texts {
+		x := (screenWidth - len(l)*fontSize) / 2
+		text.Draw(screen, l, arcadeFont, x, (i+4)*fontSize, color.White)
+	}
+
+	if gamestate <= GameReady {
+		msg := []string{
+			"MagLab by rangzen is",
+			"licenced under CC BY 3.0.",
+		}
+		for i, l := range msg {
+			x := (screenWidth - len(l)*smallFontSize) / 2
+			text.Draw(screen, l, smallArcadeFont, x, screenHeight-4+(i-1)*smallFontSize, color.White)
+		}
+	}
+
+	// Score
+	if gamestate > GameRunning {
+		scoreStr := fmt.Sprintf("Score: %d", score)
+		text.Draw(screen, scoreStr, arcadeFont, screenWidth-len(scoreStr)*fontSize-scoreMargin, fontSize+scoreMargin, color.White)
+	}
+
 	// Debug
 	pos := g.ball.Position()
 	vel := g.ball.Velocity()
@@ -315,13 +403,12 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	ebitenutil.DebugPrint(
 		screen,
 		fmt.Sprintf(
-			"Time is %5.1f\nPosition (%4.1f, %4.1f)\nVelocity (%4.1f, %4.1f)\nForce    (%4.1f, %4.1f)\nGameState: %d\nScore: %d",
+			"Time is %5.1f\nPosition (%4.1f, %4.1f)\nVelocity (%4.1f, %4.1f)\nForce    (%4.1f, %4.1f)\nGameState: %d",
 			g.time,
 			pos.X, pos.Y,
 			vel.X, vel.Y,
 			force.X, force.Y,
 			gamestate,
-			score,
 		))
 }
 
